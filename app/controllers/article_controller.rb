69e7cf4cc6948e213a591be8df8f6c25422f2ef9
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-04-17 02:05:07 +0530

    Index Action implementation - Show all Articles

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,6 +1,9 @@
 class ArticleController < ApplicationController
     def show
-        
         @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
     end
 end
