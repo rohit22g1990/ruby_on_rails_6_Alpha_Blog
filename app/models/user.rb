f34dd72d3abadd87b40a8ddf698fc8358ea0d10b
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-04-18 15:23:39 +0530

    Authenication and Add password field to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
                 uniqueness: { case_sensitive: false},
                 length: { maximum: 105 },
                 format: { with: VALID_EMAIL_ID}
+    
+    has_secure_password
+
 end
