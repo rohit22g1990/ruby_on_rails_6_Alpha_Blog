ab2ba091e5623ab3b5d98dc40318456af1ac2132
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-04-18 23:50:49 +0530

    User login functionaity and session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create 
+        user = User.where("username = ? OR email = ?", params[:session][:username], params[:session][:username]).first
+        if !user.nil? && user.authenticate(params[:session][:password])
+            session[:id] = user.id
+            flash[:notice] = "Successfully Logged in"
+            redirect_to articles_path
+        else
+            flash.now[:alert] = "Incorrect credentials"
+            render 'new'
+        end              
+    end
+
+    def destroy
+        session[:id] = nil
+        flash[:notice] = "Logged out"
+        redirect_to root_path
+    end
+end
